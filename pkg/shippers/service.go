b3418b751f2567d982689feef4ea32bd6de8060d
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-29 02:08:00 +0700

    Completed shipping service

--- pkg/shippers/service.go
+++ pkg/shippers/service.go
@@ -1,34 +1,39 @@
 package shippers
 
-import "github.com/sudhanshuraheja/tanker/pkg/appcontext"
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/sudhanshuraheja/tanker/pkg/appcontext"
+)
 
 type ShippersService interface {
-	Add(name string, machineName string) (Shipper, error)
+	Add(name string, machineName string) (int64, string, error)
 	Delete(id int64) error
-	View(id int64) ([]Shipper, error)
+	View(id int64) (Shipper, error)
 	ViewAll() ([]Shipper, error)
 }
 
 type shippersService struct {
-	ctx *appcontext.AppContext
+	ctx       *appcontext.AppContext
+	datastore ShipperDatastore
 }
 
-func NewShippersService(ctx *appcontext.AppContext) ShippersService {
-	return &shippersService{ctx}
+func NewShippersService(ctx *appcontext.AppContext, db *sqlx.DB) ShippersService {
+	datastore := NewShipperDatastore(ctx, db)
+	return &shippersService{ctx, datastore}
 }
 
-func (s *shippersService) Add(name, machineName string) (Shipper, error) {
-	return Shipper{}, nil
+func (s *shippersService) Add(name, machineName string) (int64, string, error) {
+	return s.datastore.Add(name, machineName)
 }
 
 func (s *shippersService) Delete(id int64) error {
-	return nil
+	return s.datastore.Delete(id)
 }
 
-func (s *shippersService) View(id int64) ([]Shipper, error) {
-	return nil, nil
+func (s *shippersService) View(id int64) (Shipper, error) {
+	return s.datastore.View(id)
 }
 
 func (s *shippersService) ViewAll() ([]Shipper, error) {
-	return nil, nil
+	return s.datastore.ViewAll()
 }
--- pkg/shippers/service_test.go
+++ pkg/shippers/service_test.go
@@ -1,25 +1,18 @@
 package shippers
 
 import (
-	"testing"
-
 	"github.com/sudhanshuraheja/tanker/pkg/appcontext"
 	"github.com/sudhanshuraheja/tanker/pkg/config"
 	"github.com/sudhanshuraheja/tanker/pkg/logger"
 )
 
+var shipperServiceTestContext *appcontext.AppContext
+
 func NewTestContext() *appcontext.AppContext {
-	conf := config.NewConfig()
-	log := logger.NewLogger(conf)
-	return appcontext.NewAppContext(conf, log)
-}
-
-func TestShippersServiceAdd(t *testing.T) {
-	// ctx := NewTestContext()
-	// shippersService := NewShippersService(ctx)
-
-	// _, err := shippersService.Add("test", "machine.test")
-	// assert.Nil(t, err)
-	// assert.Equal(t, "test", ship.Name)
-	// assert.Equal(t, "machine.test", ship.MachineName)
+	if shipperServiceTestContext == nil {
+		conf := config.NewConfig()
+		log := logger.NewLogger(conf)
+		shipperServiceTestContext = appcontext.NewAppContext(conf, log)
+	}
+	return shipperDatastoreTestContext
 }
