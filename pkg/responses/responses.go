9557706b29cee301e0f3caf2246438aeb34883fb
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-30 02:09:21 +0700

    Added handler for ShipperAdd

--- pkg/responses/responses.go
+++ pkg/responses/responses.go
@@ -21,3 +21,21 @@ type ErrorResponse struct {
 	Code    string `json:"code,omitempty"`
 	Message string `json:"message,omitempty"`
 }
+
+type Shipper struct {
+	ID          int64  `json:"id,omitempty"`
+	AccessKey   string `json:"access_key,omitempty"`
+	Name        string `json:"name,omitempty"`
+	MachineName string `json:"machine_name,omitempty"`
+	CreatedAt   int    `json:"created_at,omitempty"`
+	UpdatedAt   int    `json:"updated_at,omitempty"`
+}
+
+func NewShipperAddSuccessResponse(id int64, accessKey string) *Response {
+	return &Response{
+		Data: &Shipper{
+			ID:        id,
+			AccessKey: accessKey,
+		},
+	}
+}
--- pkg/shippers/handler.go
+++ pkg/shippers/handler.go
@@ -1,34 +1,55 @@
 package shippers
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/sudhanshuraheja/tanker/pkg/responses"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/sudhanshuraheja/tanker/pkg/appcontext"
-
-	"github.com/gorilla/mux"
 )
 
 type HTTPHandler func(w http.ResponseWriter, r *http.Request)
 
-type ShipperHandler struct{}
+type ShipperHandler struct {
+	service ShippersService
+}
 
-func (s *ShipperHandler) Add(ctx *appcontext.AppContext, db *sqlx.DB) HTTPHandler {
+func (s *ShipperHandler) Init(ctx *appcontext.AppContext, db *sqlx.DB) {
+	s.service = NewShippersService(ctx, db)
+}
+
+func (s *ShipperHandler) Add(ctx *appcontext.AppContext) HTTPHandler {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		fmt.Println("In the right function")
 
-		vars := mux.Vars(r)
-		fmt.Println("Vars", vars)
-		fmt.Println("Query", r.URL.Query())
-		fmt.Println("Header", r.Header)
+		name := s.parseKeyFromQuery(r, "name")
+		machineName := s.parseKeyFromQuery(r, "machineName")
 
-		// data, err := json.Marshal({})
-		// if err != nil {
-		// 	logger.Error(err)
-		// }
-		w.Write([]byte("{\"success\":\"pong\"}"))
+		id, accessKey, err := s.service.Add(name, machineName)
+		if err != nil {
+			responses.WriteJSON(
+				w, http.StatusBadRequest,
+				responses.Response{
+					Success: "false",
+					Errors: []responses.ErrorResponse{
+						responses.ErrorResponse{Code: "shipper:add:error", Message: err.Error()},
+					},
+				},
+			)
+			return
+		}
 
+		ctx.GetLogger().Infoln(id)
+		ctx.GetLogger().Infoln(accessKey)
+		responses.WriteJSON(w, http.StatusOK, responses.NewShipperAddSuccessResponse(id, accessKey))
 	}
+}
+
+func (s *ShipperHandler) parseKeyFromQuery(r *http.Request, key string) string {
+	value := ""
+	if len(r.URL.Query()[key]) > 0 {
+		value = r.URL.Query()[key][0]
+	}
+	return value
 }
