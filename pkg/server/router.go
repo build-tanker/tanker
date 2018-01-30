dd7e3cc251ba39e77275f5e7c94d6366610cd138
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-30 23:55:52 +0700

    Removed strings for rest and replaces with const

--- pkg/server/router.go
+++ pkg/server/router.go
@@ -22,25 +22,25 @@ func Router(ctx *appcontext.AppContext, db *sqlx.DB) http.Handler {
 
 	router := mux.NewRouter()
 	// GET___ .../ping
-	router.HandleFunc("/ping", pingService.Ping(ctx))
+	router.HandleFunc("/ping", pingService.Ping(ctx)).Methods(http.MethodGet)
 
 	// Shippers
 	// POST__ .../v1/shippers?name=shipper_name&machineName=machine_name
-	router.HandleFunc("/v1/shippers", shipperService.Add(ctx)).Methods("POST")
+	router.HandleFunc("/v1/shippers", shipperService.Add(ctx)).Methods(http.MethodPost)
 	// GET___ .../v1/shippers?page=1&count=25
-	router.HandleFunc("/v1/shippers", FakeHandler(ctx, db)).Methods("GET")
+	router.HandleFunc("/v1/shippers", FakeHandler(ctx, db)).Methods(http.MethodGet)
 	// GET___ .../v1/shippers/id
-	router.HandleFunc("/v1/shippers/{id}", FakeHandler(ctx, db)).Methods("GET")
+	router.HandleFunc("/v1/shippers/{id}", FakeHandler(ctx, db)).Methods(http.MethodGet)
 	// PUT___ .../v1/shippers/id?name=shipper_name&machineName=machine_name
-	router.HandleFunc("/v1/shippers/{id}", FakeHandler(ctx, db)).Methods("PUT")
+	router.HandleFunc("/v1/shippers/{id}", FakeHandler(ctx, db)).Methods(http.MethodPut)
 	// DELETE .../v1/shippers/id
-	router.HandleFunc("/v1/shippers/{id}", FakeHandler(ctx, db)).Methods("DELETE")
+	router.HandleFunc("/v1/shippers/{id}", FakeHandler(ctx, db)).Methods(http.MethodDelete)
 
 	// Builds
 	// POST__ .../v1/builds?accessKey=a1b2c3&buildSize=80&checksum=a1b2c3
-	router.HandleFunc("/v1/builds", FakeHandler(ctx, db)).Methods("POST")
+	router.HandleFunc("/v1/builds", FakeHandler(ctx, db)).Methods(http.MethodPost)
 	// POST__ .../v1/builds/abcdef?accessKey=a1b2c3
-	router.HandleFunc("/v1/builds/{id}", FakeHandler(ctx, db)).Methods("POST")
+	router.HandleFunc("/v1/builds/{id}", FakeHandler(ctx, db)).Methods(http.MethodPost)
 	return router
 }
 
