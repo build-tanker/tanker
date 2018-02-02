504d4910c263779b424e5268a15198621c83ffff
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-02-02 15:18:51 +0700

    Added empty handler for builds

--- pkg/builds/handler.go
+++ pkg/builds/handler.go
@@ -1,1 +1,48 @@
 package builds
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"source.golabs.io/core/tanker/pkg/appcontext"
+)
+
+type HTTPHandler func(w http.ResponseWriter, r *http.Request)
+
+type Handler interface {
+	Add() HTTPHandler
+}
+
+type handler struct {
+	ctx     *appcontext.AppContext
+	service Service
+}
+
+func NewHandler(ctx *appcontext.AppContext) Handler {
+	s := NewService()
+	return &handler{
+		ctx:     ctx,
+		service: s,
+	}
+}
+
+func (s *handler) Add() HTTPHandler {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (s *handler) parseKeyFromQuery(r *http.Request, key string) string {
+	value := ""
+	if len(r.URL.Query()[key]) > 0 {
+		value = r.URL.Query()[key][0]
+	}
+	return value
+}
+
+func (s *handler) parseKeyFromVars(r *http.Request, key string) string {
+	vars := mux.Vars(r)
+	fmt.Println(vars)
+	return vars[key]
+}
--- pkg/builds/handler_test.go
+++ pkg/builds/handler_test.go
@@ -1,1 +1,42 @@
 package builds
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+type MockService struct{}
+
+func NewMockService() Service {
+	return MockService{}
+}
+
+func NewTestHandler() *handler {
+	s := NewMockService()
+	ctx := NewTestContext()
+	return &handler{
+		ctx:     ctx,
+		service: s,
+	}
+}
+
+func TestAdd(t *testing.T) {
+	h := NewTestHandler()
+
+	req, err := http.NewRequest(http.MethodPost, "/v1/builds?accessKey=a1b2c3", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	response := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/builds", h.Add()).Methods(http.MethodPost)
+	router.ServeHTTP(response, req)
+
+	assert.Equal(t, 200, response.Code)
+	assert.Equal(t, "", response.Body.String())
+}
