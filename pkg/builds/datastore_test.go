02cd3a3d6cf17bdb4cee13b6f2e66e3d81d6b851
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-02-02 15:17:22 +0700

    Added datastore for builds package

--- pkg/builds/datastore_test.go
+++ pkg/builds/datastore_test.go
@@ -1,1 +1,18 @@
 package builds
+
+import (
+	"source.golabs.io/core/tanker/pkg/appcontext"
+	"source.golabs.io/core/tanker/pkg/config"
+	"source.golabs.io/core/tanker/pkg/logger"
+)
+
+var testContext *appcontext.AppContext
+
+func NewTestContext() *appcontext.AppContext {
+	if testContext == nil {
+		conf := config.NewConfig()
+		log := logger.NewLogger(conf)
+		testContext = appcontext.NewAppContext(conf, log)
+	}
+	return testContext
+}
