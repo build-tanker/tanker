17a89ac0b26f421f1a8bfa7527d37a6dc44fe755
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-02-04 07:18:35 +0700

    Added BuildsAdd to datastore

--- pkg/builds/datastore.go
+++ pkg/builds/datastore.go
@@ -1,10 +1,53 @@
 package builds
 
+import (
+	"errors"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	"source.golabs.io/core/tanker/pkg/appcontext"
+)
+
 type Build struct {
-	ID             int64  `db:"id" json:"id"`
-	BundleID       string `db:"bundle_id" json:"bundle_id"`
-	UploadComplete bool   `db:"upload_complete" json:"upload_complete"`
-	Migrated       bool   `db:"migrated" json:"migrated"`
-	CreatedAt      int    `db:"created_at" json:"created_at"`
-	UpdatedAt      int    `db:"updated_at" json:"updated_at"`
+	ID             int64     `db:"id" json:"id"`
+	Shipper        string    `db:"shipper" json:"shipper"`
+	BundleID       string    `db:"bundle_id" json:"bundle_id"`
+	UploadComplete bool      `db:"upload_complete" json:"upload_complete"`
+	Migrated       bool      `db:"migrated" json:"migrated"`
+	CreatedAt      time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
+}
+
+type Datastore interface {
+	Add(shipper string, bundleID string) (int64, error)
+}
+
+type datastore struct {
+	ctx *appcontext.AppContext
+	db  *sqlx.DB
+}
+
+func NewDatastore(ctx *appcontext.AppContext, db *sqlx.DB) Datastore {
+	return &datastore{
+		ctx: ctx,
+		db:  db,
+	}
+}
+
+func (d *datastore) Add(shipper string, bundleID string) (int64, error) {
+	rows, err := d.db.Queryx("INSERT INTO builds (shipper, bundle_id, upload_complete, migrated) VALUES($1, $2, $3, $4) RETURNING id", shipper, bundleID, false, false)
+	if err != nil {
+		return 0, err
+	}
+
+	for rows.Next() {
+		var b Build
+		err = rows.StructScan(&b)
+		if err != nil {
+			return 0, err
+		}
+		return b.ID, err
+	}
+
+	return 0, errors.New("No error in inserting, still could not find a ID")
 }
--- pkg/builds/datastore_test.go
+++ pkg/builds/datastore_test.go
@@ -1,9 +1,15 @@
 package builds
 
 import (
+	"testing"
+	"time"
+
+	sqlmock "github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 	"source.golabs.io/core/tanker/pkg/appcontext"
 	"source.golabs.io/core/tanker/pkg/config"
 	"source.golabs.io/core/tanker/pkg/logger"
+	"source.golabs.io/core/tanker/pkg/postgresmock"
 )
 
 var testContext *appcontext.AppContext
@@ -15,4 +21,20 @@ func NewTestContext() *appcontext.AppContext {
 		testContext = appcontext.NewAppContext(conf, log)
 	}
 	return testContext
+}
+
+func TestDatastoreAdd(t *testing.T) {
+	db, mock := postgresmock.NewMockSqlxDB()
+	defer postgresmock.CloseMockSqlxDB(db)
+
+	ctx := NewTestContext()
+	datastore := NewDatastore(ctx, db)
+
+	mockQuery := "^INSERT INTO builds (.+) RETURNING id$"
+	mockRows := sqlmock.NewRows([]string{"id", "shipper", "bundle_id", "upload_complete", "migrated", "created_at", "updated_at"}).AddRow(10, "testShipper77", "com.me.app", false, false, time.Now(), time.Now())
+	mock.ExpectQuery(mockQuery).WillReturnRows(mockRows)
+
+	id, err := datastore.Add("testShipper77", "com.me.app")
+	assert.Nil(t, err)
+	assert.Equal(t, int64(10), id)
 }
