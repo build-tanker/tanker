fb7ba28757ccc69866b19e0ef61f0e57170cd5e8
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-02-18 20:48:50 +0700

    Moved client id, secret and redirect url to oAuth2 instead of passing every time

--- pkg/oauth2/oauth2.go
+++ pkg/oauth2/oauth2.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/url"
+	"time"
 
 	"github.com/gojekfarm/tanker/pkg/requester"
 )
@@ -12,26 +13,42 @@ import (
 
 // OAuth2 interface to deal with OAuth2
 type OAuth2 interface {
-	GetAuthURL(clientID, redirectURL, scope, accessType, state, includeGrantedScopes, loginHint, prompt string) (string, error)
+	GetAuthURL(scope, accessType, state, includeGrantedScopes, loginHint, prompt string) (string, error)
+	GetToken(code string) error
 }
 
 type oAuth2 struct {
-	r *requester.Requester
+	r            *requester.Requester
+	clientID     string
+	clientSecret string
+	redirectURL  string
 }
 
 // NewOAuth2 - get a new client for OAuth2
-func NewOAuth2() OAuth2 {
-	return oAuth2{}
-}
+func NewOAuth2(clientID, clientSecret, redirectURL string) (OAuth2, error) {
 
-func (o oAuth2) GetAuthURL(clientID, redirectURL, scope, accessType, state, includeGrantedScopes, loginHint, prompt string) (string, error) {
 	if clientID == "" {
-		return "", errors.New("oauth2: Please enter your clientID for Google OAuth")
+		return oAuth2{}, errors.New("oauth2: Please enter your clientID for Google OAuth")
+	}
+
+	if clientSecret == "" {
+		return oAuth2{}, errors.New("oauth2: Please enter your clientSecret for Google OAuth")
 	}
 
 	if redirectURL == "" {
-		return "", errors.New("oauth2: Please enter a redirect URL")
+		return oAuth2{}, errors.New("oauth2: Please enter a redirect URL for Google Auth")
 	}
+
+	r := requester.NewRequester(2 * time.Second)
+	return oAuth2{
+		clientID:     clientID,
+		clientSecret: clientSecret,
+		redirectURL:  redirectURL,
+		r:            &r,
+	}, nil
+}
+
+func (o oAuth2) GetAuthURL(scope, accessType, state, includeGrantedScopes, loginHint, prompt string) (string, error) {
 
 	if scope == "" {
 		scope = "profile email https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
@@ -49,5 +66,9 @@ func (o oAuth2) GetAuthURL(clientID, redirectURL, scope, accessType, state, incl
 		prompt = "consent select_account"
 	}
 
-	return fmt.Sprintf("https://accounts.google.com/o/oauth2/v2/auth?scope=%s&access_type=%s&include_granted_scopes=%s&state=%s&redirect_uri=%s&response_type=code&login_hint=%s&prompt=%s&client_id=%s", url.PathEscape(scope), url.PathEscape(accessType), url.PathEscape(includeGrantedScopes), url.PathEscape(state), url.PathEscape(redirectURL), url.PathEscape(loginHint), url.PathEscape(prompt), url.PathEscape(clientID)), nil
+	return fmt.Sprintf("https://accounts.google.com/o/oauth2/v2/auth?scope=%s&access_type=%s&include_granted_scopes=%s&state=%s&redirect_uri=%s&response_type=code&login_hint=%s&prompt=%s&client_id=%s", url.PathEscape(scope), url.PathEscape(accessType), url.PathEscape(includeGrantedScopes), url.PathEscape(state), url.PathEscape(o.redirectURL), url.PathEscape(loginHint), url.PathEscape(prompt), url.PathEscape(o.clientID)), nil
+}
+
+func (o oAuth2) GetToken(code string) error {
+	return nil
 }
--- pkg/oauth2/oauth2_test.go
+++ pkg/oauth2/oauth2_test.go
@@ -7,43 +7,42 @@ import (
 )
 
 func TestGetAuthURL(t *testing.T) {
-	oa := oAuth2{}
+	oa, err := NewOAuth2("", "fakeClientSecret", "fakeRedirectURL")
+	assert.Equal(t, "oauth2: Please enter your clientID for Google OAuth", err.Error())
+
+	oa, err = NewOAuth2("fakeClientID", "fakeClientSecret", "")
+	assert.Equal(t, "oauth2: Please enter a redirect URL for Google Auth", err.Error())
+
+	oa, err = NewOAuth2("fakeClientID", "fakeClientSecret", "fakeRedirectURL")
+	assert.Nil(t, err)
 
 	// Get URL
-	url, err := oa.GetAuthURL("fakeClientID", "fakeRedirectURL", "fakeScope", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
+	url, err := oa.GetAuthURL("fakeScope", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth?scope=fakeScope&access_type=offline&include_granted_scopes=true&state=fakeState&redirect_uri=fakeRedirectURL&response_type=code&login_hint=fakeLoginHint&prompt=fakePrompt&client_id=fakeClientID", url)
 
-	// Try with no clientID
-	url, err = oa.GetAuthURL("", "fakeRedirectURL", "fakeScope", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
-	assert.Equal(t, "oauth2: Please enter your clientID for Google OAuth", err.Error())
-
-	// Try with no Redirect URL
-	url, err = oa.GetAuthURL("fakeClientID", "", "fakeScope", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
-	assert.Equal(t, "oauth2: Please enter a redirect URL", err.Error())
-
 	// Try with no scope
-	url, err = oa.GetAuthURL("fakeClientID", "fakeRedirectURL", "", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
+	url, err = oa.GetAuthURL("", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20email%20https:%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https:%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&access_type=offline&include_granted_scopes=true&state=fakeState&redirect_uri=fakeRedirectURL&response_type=code&login_hint=fakeLoginHint&prompt=fakePrompt&client_id=fakeClientID", url)
 
 	// Try with no include_granted_scopes
-	url, err = oa.GetAuthURL("fakeClientID", "fakeRedirectURL", "fakeScope", "fakeAccessType", "fakeState", "", "fakeLoginHint", "fakePrompt")
+	url, err = oa.GetAuthURL("fakeScope", "fakeAccessType", "fakeState", "", "fakeLoginHint", "fakePrompt")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth?scope=fakeScope&access_type=offline&include_granted_scopes=true&state=fakeState&redirect_uri=fakeRedirectURL&response_type=code&login_hint=fakeLoginHint&prompt=fakePrompt&client_id=fakeClientID", url)
 
 	// Try with online accessType
-	url, err = oa.GetAuthURL("fakeClientID", "fakeRedirectURL", "fakeScope", "online", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
+	url, err = oa.GetAuthURL("fakeScope", "online", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth?scope=fakeScope&access_type=online&include_granted_scopes=true&state=fakeState&redirect_uri=fakeRedirectURL&response_type=code&login_hint=fakeLoginHint&prompt=fakePrompt&client_id=fakeClientID", url)
 
 	// Try with empty accessType
-	url, err = oa.GetAuthURL("fakeClientID", "fakeRedirectURL", "fakeScope", "", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
+	url, err = oa.GetAuthURL("fakeScope", "", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "fakePrompt")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth?scope=fakeScope&access_type=offline&include_granted_scopes=true&state=fakeState&redirect_uri=fakeRedirectURL&response_type=code&login_hint=fakeLoginHint&prompt=fakePrompt&client_id=fakeClientID", url)
 
 	// Try with empty prompt
-	url, err = oa.GetAuthURL("fakeClientID", "fakeRedirectURL", "fakeScope", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "")
+	url, err = oa.GetAuthURL("fakeScope", "fakeAccessType", "fakeState", "fakeIncludeGrantedScopes", "fakeLoginHint", "")
 	assert.Nil(t, err)
 	assert.Equal(t, "https://accounts.google.com/o/oauth2/v2/auth?scope=fakeScope&access_type=offline&include_granted_scopes=true&state=fakeState&redirect_uri=fakeRedirectURL&response_type=code&login_hint=fakeLoginHint&prompt=consent%20select_account&client_id=fakeClientID", url)
 }
