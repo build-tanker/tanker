8a7cc5110a08f3170d10c33b5fde12710b69071a
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-02-24 14:49:17 +0700

    Planned paths required in the router

--- pkg/server/router.go
+++ pkg/server/router.go
@@ -26,7 +26,31 @@ func Router(ctx *appcontext.AppContext, db *sqlx.DB) http.Handler {
 	// GET___ .../ping
 	router.HandleFunc("/ping", pingHandler.Ping(ctx)).Methods(http.MethodGet)
 
-	// Shippers
+	// Auth
+	// POST .../v1/users source=google&access_token=tkn&name=name&email=email&user_id=123
+	// GET .../v1/users/15
+	// PUT .../v1/users/15 access_token=tkn&name=name&deleted=true
+	// DELETE .../v1/users/15
+
+	// AppGroup
+	// POST .../v1/appGroup name=name
+	// GET .../v1/appGroup/15
+	// PUT .../v1/appGroup/15 name=name
+	// DELETE .../v1/appGroup/15
+
+	// App
+	// POST ../v1/app appGroup=appGroup&name=name&bundleId=bundle_id&platform=platform
+	// GET .../v1/app/15
+	// PUT .../v1/app/15 name=name
+	// DELETE .../v1/app/15
+
+	// Access
+	// POST ../v1/access person=person&appGroup=app_group&app=app&access_level=admin&access_given_by=person
+	// GET ../v1/access/15
+	// PUT ../v1/access/15 access_level=admin
+	// DELETE ../v1/access/15
+
+	// Shipper
 	// POST__ .../v1/shippers?name=shipper_name&machineName=machine_name
 	router.HandleFunc("/v1/shippers", shipperHandler.Add()).Methods(http.MethodPost)
 	// GET___ .../v1/shippers?page=1&count=25
@@ -38,7 +62,7 @@ func Router(ctx *appcontext.AppContext, db *sqlx.DB) http.Handler {
 	// DELETE .../v1/shippers/id
 	router.HandleFunc("/v1/shippers/{accessKey}", shipperHandler.Delete()).Methods(http.MethodDelete)
 
-	// Builds
+	// Build
 	// POST__ .../v1/builds?accessKey=a1b2c3&bundle=com.me.app
 	router.HandleFunc("/v1/builds", buildsHandler.Add()).Methods(http.MethodPost)
 	return router
